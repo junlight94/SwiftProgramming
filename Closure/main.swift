07dc5dd0edd2cab3138a44e15fa44a4994bcd49a
JunYoung Lee <52552781+junlight94@users.noreply.github.com>
2022-07-04 22:05:27 +0900

    클로저 완료

--- Closure/main.swift
+++ Closure/main.swift
@@ -123,3 +123,171 @@ let amReversed: [String] = names.sorted {$0 > $1}
  여기서 > 자체가 함수의 이름입니다. 더군다나 이 함수는 우리가 전달인자로 보내기에 충분한 조건을 갖고 있습니다.
  아직 제네릭과 프로토콜을 배우지 않아 다 이해할 순 없지만 T를 String으로만 바꿔서 생각하면 됩니다.
  */
+
+//MARK: 값 획득
+/*
+ 클로저는 자신이 정의된 위치의 주변 문맥을 통해 상수나 변수를 획득할 수 있습니다.
+ 클로저는 비동기 작업에 많이 사용됩니다. 클로저를 통해 비동기 콜백을 작성하는 경우,
+ 현재 상태를 미리 획득해두지 않으면, 실제로 클로저의 기능을 실행하는 순간에는 주변의 상수나 변수가 이미 메모리에 존재하지 않는 경우가 발생합니다.
+ incrementer라는 함수를 중첩 함수로 포함하는 makeIncrementer 함수를 살펴보겠습니다.
+ 중첩함수인 incrementer()함수는 자신 주변에 있는 runningTotal과 amount라는 두 값을 획득합니다.
+ 두값을 획득한 후에 incrementer는 클로저로서 makeIncrementer 함수에 의해 반환됩니다.
+ */
+func makeIncrementer(forIncrement amount: Int) -> (() -> Int) {
+    var runningTotal = 0
+    func incrementer() -> Int {
+        runningTotal += amount
+        return runningTotal
+    }
+    return incrementer
+}
+
+//MARK: 클로저는 참조 타입
+/*
+ 함수나 클로저를 상수나 변수에 할당할 때마다 사실은 상수나 변수에 함수나 클로저의 참조를 설정하는 것입니다.
+ 클로저의 참조를 다른 상수에 할당해준다면 이는 두 상수가 모두 같은 클로저를 가리킨다는 뜻 입니다.
+ */
+
+let incrementByTwo:(() -> Int) = makeIncrementer(forIncrement: 2)
+let sameWithIncrementByTwo: (() -> Int) = incrementByTwo
+
+let first: Int = incrementByTwo()           // 2
+let second: Int = sameWithIncrementByTwo()  // 4
+
+
+//MARK: 탈출 클로저
+/*
+ 함수의 전달인자로 전달한 클로저가 함수 종료 후에 호출될 때 클로저가 함수를 탈출한다고 표현합니다.
+ 클로저를 매개변수로 갖는 함수를 선언할 때 매개변수 이름의 콜론 뒤에 @escaping 키워드를 사용하여
+ 클로저가 탈출하는 것을 허용한다고 명시해줄 수 있습니다.
+ */
+
+
+typealias VoidVoidClosure = () -> Void
+let firstClosure: VoidVoidClosure = {
+    print("Closure A")
+}
+let secondClosure: VoidVoidClosure = {
+    print("Closure B")
+}
+
+// first와 second 매개변수 클로저는 함수의 반환 값으로 사용될 수 있으므로 탈출 클로저입니다.
+func returnOneClosure(first: @escaping VoidVoidClosure, second: @escaping VoidVoidClosure, shouldReturnFirstClosure: Bool) -> VoidVoidClosure{
+    // 전달인자로 전달받은 클로저를 함수 외부로 다시 반환하기 때문에 함수를 탈출하는 클로저입니다.
+    return shouldReturnFirstClosure ? first : second
+}
+
+// 함수에서 반환한 클로저가 함수 외부의 상수에 저장되었습니다.
+let returnedClosure: VoidVoidClosure = returnOneClosure(first: firstClosure, second: secondClosure, shouldReturnFirstClosure: true)
+
+returnedClosure() // Closure A
+
+var closures: [VoidVoidClosure] = []
+
+// closure 매개변수 클로저는 함수 외부의 변수에 저장될 수 있으므로 탈출 클로저입니다.
+func appendClosure(closure:@escaping VoidVoidClosure) {
+    // 전달인자로 전달받은 클로저가 함수 외부의 변수 내부에 저장되므로 함수를 탈출합니다.
+    closures.append(closure)
+}
+
+// 클래스 인스턴스 메서드에 사용되는 탈출, 비탈출 클로저
+typealias VoidClosure = () -> Void
+
+func functionWithNoescapeClosure(closure: VoidClosure){
+    closure()
+}
+
+func functionWithEscapingClosure(completionHandler: @escaping VoidClosure) -> VoidClosure {
+    return completionHandler
+}
+
+class SomeClass {
+    var x = 10
+    
+    func runNoescapeClosure() {
+        // 비탈출 클로저에서 self 키워드 사용은 선택 사항입니다.
+        functionWithNoescapeClosure { x = 200 }
+    }
+    
+    func runEscapeClosure() -> VoidClosure {
+        // 탈출 클로저에서는 명시적으로 self를 사용해야 합니다.
+        functionWithEscapingClosure { self.x = 100 }
+    }
+}
+
+let instance: SomeClass = SomeClass()
+instance.runNoescapeClosure()
+print(instance.x)
+
+let returnedClosure2: VoidClosure = instance.runEscapeClosure()
+returnedClosure2()
+print(instance.x)
+
+//MARK: withoutActuallyEscaping
+/*
+ 비탈출 클로저로 전달한 클로저가 탈출 클로저인 척 해야 하는 경우.
+ 실제로 탈출하지 않는데 다른 함수에서 탈출 클로저를 요구하는 상황인 경우
+ */
+ 
+let numbers:[Int] = [2, 4, 6, 8]
+
+let evenNumberPredicate = { (number: Int) -> Bool in
+    return number % 2 == 0
+}
+
+let oddNumberPredicate = { (number: Int) -> Bool in
+    return number % 2 == 1
+}
+
+func hasElements(in array: [Int], match predicate: (Int) -> Bool) -> Bool {
+    return withoutActuallyEscaping(predicate, do: { escapablePredicate in
+        return (array.lazy.filter { escapablePredicate($0) }.isEmpty == false)
+    })
+}
+
+let hasEvenNumber = hasElements(in: numbers, match: evenNumberPredicate)
+let hasOddNumber = hasElements(in: numbers, match: oddNumberPredicate)
+
+print(hasEvenNumber) // true
+print(hasOddNumber)  // false
+
+//MARK: 자동 클로저
+/*
+ 함수의 전달인자로 전달하는 표현을 자동으로 변환해주는 클로저를 자동 클로저라고 합니다.
+ 자동 클로저는 전달인자를 갖지 않습니다. 호출되었을 때 자신이 감싸고 있는 코드의 결괏값을 반환합니다.
+ */
+
+// 클로저를 이용한 연산 지연
+// 대기 중인 손님들입니다.
+var customersInLine: [String] = ["동훈", "용덕", "선우", "사랑"]
+print(customersInLine.count) // 4
+
+// 클로저를 만들어두면 클로저 내부의 코드를 미리 실행하지 않고 가지고만 있습니다.
+let customerProvider: () -> String = {
+    return customersInLine.removeFirst()
+}
+print(customersInLine.count) // 4
+
+// 실제로 실행합니다.
+print("\(customerProvider())님 입장하시면 됩니다.")
+print(customersInLine.count) // 3
+
+// 함수의 전달인자로 전달하는 클로저
+func serveCustomer(_ customerProvider: () -> String) {
+    print("\(customerProvider())님 입장하시면 됩니다.")
+}
+serveCustomer({ customersInLine.removeFirst() })
+
+
+// 자동 클로저의 사용
+func serveAuto(_ customerProvider: @autoclosure() -> String) {
+    print("\(customerProvider())님 입장하시면 됩니다.")
+}
+serveAuto(customersInLine.removeFirst())
+
+// 자동 클로저의 탈출
+func returnProvider(_ customerProvider: @autoclosure @escaping() -> String) ->(() -> String) {
+    return customerProvider
+}
+let customerAuto: () -> String = returnProvider(customersInLine.removeFirst())
+print("\(customerAuto())님 입장하시면 됩니다.")
